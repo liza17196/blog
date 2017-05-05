7e1da62a2f86be593fd33dc28c5af69254193539
liza17196 <lizaqwerty.software@gmail.com>
2017-05-05 13:07:15 +0300

    with new routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,16 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::auth();
 
-Route::get('/', function () {
-    return view('pages/index');
-});
+// Authentication Routes...
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
+
+// Registration Routes...
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+
 Route::get('/', 'SectionController@index');					
 Route::get('/sections', 'SectionController@index');				//главные страницы
 
